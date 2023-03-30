4d907645b764d031547227a39e8374ab655ef52c
Kristian Mikkonen <domainpro72@gmail.com>
2023-03-30 17:43:39 +0000

    Add event listeners for document loading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+
+})
+
 function runGame() {
 
 }
@@ -10,10 +19,14 @@ function calculateCorrectAnswer() {
     
 }
 
-function incrementScore() {
+function incrementYourScore() {
     
 }
 
-function incrementWrongAnswer() {
+function incrementComputerScore() {
+    
+}
+
+function displayWinner() {
     
 }
