fa24656f4fe56778534859fb899052a5e2918b12
Kristian Mikkonen <domainpro72@gmail.com>
2023-04-01 12:15:27 +0000

    Add DOM elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,12 @@
 // Wait for the DOM to finish loading before running the game
-// Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
-
-
-})
-
-function runGame() {
-
-}
-
-function checkAnswer() {
-    
-}
-
-function calculateCorrectAnswer() {
-    
-}
-
-function incrementYourScore() {
-    
-}
-
-function incrementComputerScore() {
-    
-}
-
-function displayWinner() {
-    
-}
+// CODE CREDIT: Web Development Tutorial - JavaScript, HTML, CSS - Rock Paper Scissors Game by whatsdev -  https://www.freecodecamp.org - https://www.youtube.com/watch?v=jaVNP3nIAv0&t=2010s
+const yourScore = 0;
+const computerScore = 0;
+const yourScore_span = document.getElementById("your-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
